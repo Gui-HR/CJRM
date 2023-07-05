489ec298c12942694608db67335b68518790ea04
Gui-HR <guilhermehuertar@gmail.com>
2023-07-05 16:35:38 -0300

    Aula 1

---
+++ aula-01/app.js
@@ -0,0 +1,22 @@
+// Comentario de uma linha.
+
+/*
+ Comentario de
+ mais de 
+ uma linha
+*/
+
+let age = 19
+let myName = 'Guilherme'
+console.log(age, myName)
+
+age = 20
+console.log(age)
+
+const points = 100
+console.log(points)
+
+var score = 75
+score = 50
+console.log(score)
+
