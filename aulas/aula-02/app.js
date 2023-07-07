1f077f96efcbc97a6a9203178f6e352a893115f6
Gui-HR <guilhermehuertar@gmail.com>
2023-07-07 12:22:49 -0300

    Aula02  Numbers

--- aulas/aula-02/app.js
+++ aulas/aula-02/app.js
@@ -1,3 +1,4 @@
+/*
 console.log('hello, world')
 
 const email = 'guilhermehuertar@gmail.com'
@@ -20,4 +21,25 @@ const result = fullName.toLowerCase()
 console.log(result, fullName)
 
 console.log(email.indexOf('h'));
+*/
+
+/* 
+const email = 'pedrin_rei_delas@hotmail.com.br'
+const lastIndexOfI = email.lastIndexOf('i')
+console.log(lastIndexOfI)
+
+const emailSlice = email.slice(11, 16)
+console.log(emailSlice)
+
+const newEmail = email.replace('pedrin_rei_delas', 'pieck.finger')
+console.log(newEmail)
+*/
+
+const radius = 10
+const pi = 3.14
+
+const area = pi * radius ** 2
+console.log(area)
+
+console.log(5 % 2)
 
