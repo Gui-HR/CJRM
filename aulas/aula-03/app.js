7c1437cf3fc537a0eede1166364fe8e421e84649
Gui-HR <guilhermehuertar@gmail.com>
2023-07-11 12:29:28 -0300

    Aula 03-02

--- aulas/aula-03/app.js
+++ aulas/aula-03/app.js
@@ -1,3 +1,9 @@
 const cores = ['Amarelo', 'Azul', 'Vermelho']
 const numeros = [1, 2, 3, 4, 5, 6, 7]
 const misturado = ['Uva', 'Monopoli', 73, 'Emerson', 9]
+
+const random = cores.concat('vaca', 'sombrancelha', 'batata frita')
+console.log(cores.length)
+console.log(numeros.join('<'))
+console.log(misturado.indexOf('Monopoli'))
+console.log(random)
