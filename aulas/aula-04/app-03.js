56044ac8404494f1cdebb28fe95940f868862460
Gui-HR <guilhermehuertar@gmail.com>
2023-07-12 17:17:23 -0300

    Aula 04-04

---
+++ aulas/aula-04/app-03.js
@@ -0,0 +1,10 @@
+let score = '100'
+score = Number(score) + 1
+
+console.log(score, typeof score)
+
+let crazyConvertion = String(10)
+console.log(crazyConvertion, typeof crazyConvertion)
+
+const booleanConvertion = Boolean(crazyConvertion)
+console.log(booleanConvertion, typeof booleanConvertion)
