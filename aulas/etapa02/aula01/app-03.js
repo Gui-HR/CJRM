5a0f1b592bd6f55accba282e25db2e33a5e734a4
Gui-HR <guilhermehuertar@gmail.com>
2023-07-14 22:47:56 -0300

    Aula01-03 Etapa02

---
+++ aulas/etapa02/aula01/app-03.js
@@ -0,0 +1,13 @@
+/*
+for (let i = 0; i < 5; i++) {
+    console.log(`${i}º Loop`)
+}
+
+console.log('Acabaram os loops')
+*/
+
+const pokemons = ['Mew', 'Zoroark', 'Sandile', 'Goodra']
+
+for (let i = 0 ; i < pokemons.length; i++) {
+    console.log(pokemons[i])
+}
